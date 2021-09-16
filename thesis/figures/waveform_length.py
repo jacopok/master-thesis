c1f1ba56eb4236dd6dd2720626d7f7c20d6f4c0e
jacopo <jacopok@gmail.com>
2021-09-16 22:40:44 +0200

    added a new figure

--- thesis/figures/waveform_length.py
+++ thesis/figures/waveform_length.py
@@ -3,6 +3,7 @@
 from mlgw_bns.utils import *
 import matplotlib.pyplot as plt
 from matplotlib import rc
+import matplotlib as mpl
 rc('font',**{'family':'serif','serif':['Palatino']})
 rc('text', usetex=True)
 rc('text.latex', preamble=r'''\usepackage{amsmath}
@@ -10,13 +11,17 @@ rc('text.latex', preamble=r'''\usepackage{amsmath}
           \usepackage{siunitx}
           ''')
 
-f0s = np.logspace(.5, 1.7, num=5000) * u.Hz
+f0s = np.logspace(.6, 1.7, num=5000) * u.Hz
 
 nyquist = 2 ** 11 * u.Hz
-times = (1/ optimal_df_hz(f0s)).to(u.min)
+times = (1 / optimal_df(f0s)).to(u.min)
+times_not_round = (1 / optimal_df(f0s, power_of_two=False)).to(u.min)
 
 plt.loglog(f0s, times, c='black')
+plt.loglog(f0s, times_not_round, c='black', ls='--')
+
 ax = plt.gca()
+
 ax2 = ax.twinx()
 
 ax2.loglog(f0s, (times * nyquist).si, alpha=0.)
@@ -28,6 +33,8 @@ ax2.set_ylabel(f'Number of points at {2*nyquist:.0f} sampling')
 ax.grid('on')
 
 ax.set_title('Duration of waveforms')
+ax.xaxis.set_major_formatter(mpl.ticker.EngFormatter())
+ax.xaxis.set_minor_formatter(mpl.ticker.EngFormatter())
 
 plt.savefig('waveform_length.pdf', dpi=150)
 # %%
